333168a9315224b23b25d302696cc9326640e570
CristianTacoronteRivero <cristiantr.develop@gmail.com>
2022-09-28 18:43:04 +0100

    actualizacion dependencias y preprocessing

---
+++ modeltools/preprocessing.py
@@ -0,0 +1,8 @@
+"""
+Este modulo contiene helpers para el preprocesamiento de datos
+"""
+import numpy as np
+import pandas as pd
+
+def get_numerical_feature(df):
+    return df.select_dtypes(include=[np.number]).columns
