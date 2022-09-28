e777affec803794f7e5f40a47c7bf29be8872c79
CristianTacoronteRivero <cristiantr.develop@gmail.com>
2022-09-28 20:12:40 +0100

    build.yml

--- modeltools/preprocessing.py
+++ modeltools/preprocessing.py
@@ -2,7 +2,7 @@
 Este modulo contiene helpers para el preprocesamiento de datos
 """
 import numpy as np
-import pandas as pd
+# import pandas as pd
 
 def get_numerical_feature(df):
     return df.select_dtypes(include=[np.number]).columns
