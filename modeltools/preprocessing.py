c29388fc9fb94f1cd5a0cfec8beb195db284f2ac
CristianTacoronteRivero <cristiantr.develop@gmail.com>
2022-10-10 17:23:10 +0100

    commit sin error

--- modeltools/preprocessing.py
+++ modeltools/preprocessing.py
@@ -1,23 +1,11 @@
 """
 Este modulo contiene helpers para el preprocesamiento de datos
 """
+
+
 import numpy as np
 
 # import pandas as pd
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 def get_numerical_feature(data_frame):
